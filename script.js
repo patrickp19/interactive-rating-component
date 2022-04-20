852e28011f1690ff8bbac4dddaff97f13d94a5e2
patrickp19 <patrick212800@hotmail.com>
2022-04-20 01:44:40 -0600

    v1

---
+++ script.js
@@ -0,0 +1,28 @@
+submitBtn = document.getElementById("submit");
+ratingState = document.getElementById("rating-state");
+thankYouState = document.getElementById("thank-you-state");
+// ratingGroupValue = document.querySelector(
+//   'input[name="rating-group"]:checked'
+// ).value;
+ratingMessage = document.getElementById("rating-message");
+
+// submitBtn.onClick(() => {
+//     ratingState.
+
+// });
+
+// console.log(ratingGroupValue);
+function submit() {
+  if (
+    document.querySelector('input[name="rating-group"]:checked').value != null
+  ) {
+    console.log(
+      document.querySelector('input[name="rating-group"]:checked').value
+    );
+    ratingMessage.innerText = document.querySelector(
+      'input[name="rating-group"]:checked'
+    ).value;
+    ratingState.style.display = "none";
+    thankYouState.style.display = "flex";
+  }
+}
